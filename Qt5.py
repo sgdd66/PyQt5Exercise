53476ac06c15d76a48ff24d9feacb41608ff350f
SGD <1770480953@qq.com>
2017-10-09 22:59:53 +0800

    test5-test26
    PyQt5测试案例

--- Qt5.py
+++ Qt5.py
@@ -2,10 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtWidgets import QApplication,QWidget,QToolTip,QPushButton
-from PyQt5.QtGui import QIcon
-from PyQt5.QtGui import QFont
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtWidgets import QApplication,QWidget,QToolTip,QFrame,\
+    QDesktopWidget,QMainWindow,QAction,qApp,QSizePolicy,\
+    QHBoxLayout,QVBoxLayout,QGridLayout,\
+    QLabel,QLineEdit,QTextEdit,QSlider,QLCDNumber,QPushButton, \
+    QMessageBox,QInputDialog,QColorDialog,QFontDialog,QFileDialog,\
+    QCheckBox,QSlider,QProgressBar,QCalendarWidget,QSplitter,QComboBox
+from PyQt5.QtGui import QIcon,QFont,QColor,QPixmap
+from PyQt5.QtCore import QCoreApplication,Qt,pyqtSignal,QObject,QBasicTimer,QDate
+
 
 #测试1：生成一个简单窗口
 def test1():
@@ -34,12 +39,6 @@ class Example2(QWidget):
 
         self.show()
 
-def test2():
-
-    app=QApplication(sys.argv)
-    ex=Example2()
-    sys.exit(app.exec_())
-
 #测试3：显示提示文本
 class Example3(QWidget):
 
@@ -62,11 +61,6 @@ class Example3(QWidget):
 
         self.show()
 
-def test3():
-    app = QApplication(sys.argv)
-    ex = Example3()
-    sys.exit(app.exec_())
-
 #测试4：点击按钮关闭窗口
 class Example4(QWidget):
     def __init__(self):
@@ -84,11 +78,732 @@ class Example4(QWidget):
         self.setWindowTitle('Quit button')
         self.show()
 
+#测试5：对话框与窗口居中
+class Example5(QWidget):
+    def __init__(self):
+        super().__init__()
 
-def test4():
-    app = QApplication(sys.argv)
-    ex = Example4()
-    sys.exit(app.exec_())
+        self.initUI()
+
+    def initUI(self):
+
+        self.setGeometry(300, 300, 250, 150)
+        self.center()
+        self.setWindowTitle('Message box')
+        self.show()
+
+    def closeEvent(self, event):
+
+        reply = QMessageBox.question(self, 'Message1',
+                                     "Are you sure to quit?", QMessageBox.Yes |
+                                     QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    def center(self):
+
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+#测试6：菜单和工具栏
+class Example6(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        exitAction = QAction(QIcon('2.jpg'), '&Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
+
+        # 菜单栏
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+        #工具栏
+        self.toolbar = self.addToolBar('Exit')
+        self.toolbar.addAction(exitAction)
+
+        self.statusBar().showMessage('Ready')
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Statusbar')
+        self.show()
+
+#测试7：箱布局
+class Example7(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        okButton = QPushButton("OK")
+        cancelButton = QPushButton("Cancel")
+        okButton.clicked.connect(QCoreApplication.quit)
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(5)
+        hbox.addWidget(okButton)
+        hbox.addWidget(cancelButton)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+
+        self.setLayout(vbox)
+
+        self.setGeometry(300, 300, 300, 150)
+        self.setWindowTitle('Buttons')
+        self.show()
+
+#测试8：网格布局
+class Example8(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+        names = ['Cls', 'Bck', '', 'Close',
+                 '7', '8', '9', '/',
+                 '4', '5', '6', '*',
+                 '1', '2', '3', '-',
+                 '0', '.', '=', '+']
+
+        positions = [(i, j) for i in range(5) for j in range(4)]
+
+        for position, name in zip(positions, names):
+
+            if name == '':
+                continue
+            button = QPushButton(name)
+            grid.addWidget(button, position[0],position[1])
+            print(position)
+
+        self.move(300, 150)
+        self.setWindowTitle('Calculator')
+        self.show()
+
+#测试9：网格跨行布局
+class Example9(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        title = QLabel('Title')
+        author = QLabel('Author')
+        review = QLabel('Review')
+
+        titleEdit = QLineEdit()
+        authorEdit = QLineEdit()
+        reviewEdit = QTextEdit()
+
+        grid = QGridLayout()
+        grid.setSpacing(10)
+
+        grid.addWidget(title, 1, 0)
+        grid.addWidget(titleEdit, 1, 1)
+
+        grid.addWidget(author, 2, 0)
+        grid.addWidget(authorEdit, 2, 1)
+
+        grid.addWidget(review, 3, 0)
+        grid.addWidget(reviewEdit, 3, 1, 5, 1)
+
+        self.setLayout(grid)
+
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle('Review')
+        self.show()
+
+#测试10：信号与槽
+class Example10(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        lcd = QLCDNumber(self)
+        sld = QSlider(Qt.Horizontal, self)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(lcd)
+        vbox.addWidget(sld)
+
+        self.setLayout(vbox)
+        sld.valueChanged.connect(lcd.display)
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Signal & slot')
+        self.show()
+
+#测试11：重写事件处理函数
+class Example11(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Event handler')
+        self.show()
+
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Escape:
+            self.close()
+
+#测试12：事件发送者
+class Example12(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        btn1 = QPushButton("Button 1", self)
+        btn1.move(30, 50)
+
+        btn2 = QPushButton("Button 2", self)
+        btn2.move(150, 50)
+
+        btn1.clicked.connect(self.buttonClicked)
+        btn2.clicked.connect(self.buttonClicked)
+
+        self.statusBar()
+
+        self.setGeometry(300, 300, 290, 150)
+        self.setWindowTitle('Event sender')
+        self.show()
+
+    def buttonClicked(self):
+        sender = self.sender()
+        self.statusBar().showMessage(sender.text() + ' was pressed')
+
+#测试13：自定义信号
+
+class Example13(QMainWindow):
+
+    closeApp = pyqtSignal([str], [int])
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        self.closeApp[int].connect(self.myShow)
+        self.setGeometry(300, 300, 290, 150)
+        self.setWindowTitle('Emit signal')
+        self.show()
+
+    def myShow(self,i):
+        self.statusBar().showMessage('{0}'.format(type(i)))
+        # self.statusBar().showMessage(i)
+    def mousePressEvent(self, event):
+        self.closeApp[int].emit('1')
+
+class SinClass(QObject):
+    ##声明一个无参数的信号
+    sin1 = pyqtSignal()
+
+    ##声明带一个int类型参数的信号
+    sin2 = pyqtSignal(int)
+
+    ##声明带一个int和str类型参数的信号
+    sin3 = pyqtSignal(int, str)
+
+    ##声明带一个列表类型参数的信号
+    sin4 = pyqtSignal(list)
+
+    ##声明带一个字典类型参数的信号
+    sin5 = pyqtSignal(dict)
+
+    ##声明一个多重载版本的信号，包括了一个带int和str类型参数的信号，以及带str参数的信号
+    sin6 = pyqtSignal([int, str], [str])
+
+    def __init__(self, parent=None):
+        super(SinClass,self).__init__(parent)
+
+        ##信号连接到指定槽
+        self.sin1.connect(self.sin1Call)
+        self.sin2.connect(self.sin2Call)
+        self.sin3.connect(self.sin3Call)
+        self.sin4.connect(self.sin4Call)
+        self.sin5.connect(self.sin5Call)
+        self.sin6[int, str].connect(self.sin6Call)
+        self.sin6[str].connect(self.sin6OverLoad)
+
+        ##信号发射
+        self.sin1.emit()
+        self.sin2.emit(1)
+        self.sin3.emit(1, "text")
+        self.sin4.emit([1, 2, 3, 4])
+        self.sin5.emit({"name": "codeio", "age": "25"})
+        self.sin6[int, str].emit(1, "text")
+        self.sin6[str].emit("text")
+
+    def sin1Call(self):
+        print("sin1 emit")
+
+    def sin2Call(self, val):
+        print("sin2 emit,value:", val)
+
+    def sin3Call(self, val, text):
+        print("sin3 emit,value:", val, text)
+
+    def sin4Call(self, val):
+        print("sin4 emit,value:", val)
+
+    def sin5Call(self, val):
+        print("sin5 emit,value:", val)
+
+    def sin6Call(self, val, text):
+        print("sin6 emit,value:", val, text)
+
+    def sin6OverLoad(self, val):
+        print("sin6 overload emit,value:", val)
+
+#测试14：输入对话框
+class Example14(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.btn = QPushButton('Dialog', self)
+        self.btn.move(20, 20)
+        self.btn.clicked.connect(self.showDialog)
+
+        self.le = QLineEdit(self)
+        self.le.move(130, 22)
+
+        self.setGeometry(300, 300, 290, 150)
+        self.setWindowTitle('Input dialog')
+        self.show()
+
+    def showDialog(self):
+        text, ok = QInputDialog.getText(self, 'Input Dialog',
+                                        'Enter your name:')
+
+        if ok:
+            self.le.setText(str(text))
+
+#测试15：颜色选择对话框
+class Example15(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        col = QColor(0, 0, 0)
+
+        self.btn = QPushButton('Dialog', self)
+        self.btn.move(20, 20)
+
+        self.btn.clicked.connect(self.showDialog)
+
+        self.frm = QFrame(self)
+        self.frm.setStyleSheet("QWidget { background-color: %s }"
+                               % col.name())
+        self.frm.setGeometry(130, 22, 100, 100)
+
+        self.setGeometry(300, 300, 250, 180)
+        self.setWindowTitle('Color dialog')
+        self.show()
+
+    def showDialog(self):
+        col = QColorDialog.getColor()
+
+        if col.isValid():
+            self.frm.setStyleSheet("QWidget { background-color: %s }"
+                                   % col.name())
+
+#测试16：字体选择对话框
+class Example16(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        vbox = QVBoxLayout()
+
+        btn = QPushButton('Dialog', self)
+        btn.setSizePolicy(QSizePolicy.Fixed,
+                          QSizePolicy.Fixed)
+
+
+
+        vbox.addWidget(btn)
+
+        btn.clicked.connect(self.showDialog)
+
+        self.lbl = QLabel('Knowledge only matters', self)
+
+        vbox.addWidget(self.lbl)
+        self.setLayout(vbox)
+
+        self.setGeometry(300, 300, 250, 180)
+        self.setWindowTitle('Font dialog')
+        self.show()
+
+    def showDialog(self):
+        font, ok = QFontDialog.getFont()
+        if ok:
+            self.lbl.setFont(font)
+
+#测试17：文件选择对话框
+class Example17(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.textEdit = QTextEdit()
+        self.setCentralWidget(self.textEdit)
+        self.statusBar()
+
+        openFile = QAction(QIcon('open.png'), 'Open', self)
+        openFile.setShortcut('Ctrl+O')
+        openFile.setStatusTip('Open new File')
+        openFile.triggered.connect(self.showDialog)
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(openFile)
+
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle('File dialog')
+        self.show()
+
+    def showDialog(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')
+
+        if fname[0]:
+            f = open(fname[0], 'r')
+
+            with f:
+                data = f.read()
+                self.textEdit.setText(data)
+
+#测试18：复选框
+class Example18(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        cb = QCheckBox('Show title', self)
+        cb.move(20, 20)
+        cb.toggle()
+        cb.stateChanged.connect(self.changeTitle)
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('QCheckBox')
+        self.show()
+
+    def changeTitle(self, state):
+
+        if state == Qt.Checked:
+            self.setWindowTitle('QCheckBox')
+        else:
+            self.setWindowTitle('')
+
+#测试19：切换按钮
+class Example19(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        self.col = QColor(0, 0, 0)
+
+        redb = QPushButton('Red', self)
+        redb.setCheckable(True)
+        redb.move(10, 10)
+
+        redb.clicked[bool].connect(self.setColor)
+
+        greenb = QPushButton('Green', self)
+        greenb.setCheckable(True)
+        greenb.move(10, 60)
+
+        greenb.clicked[bool].connect(self.setColor)
+
+        blueb = QPushButton('Blue', self)
+        blueb.setCheckable(True)
+        blueb.move(10, 110)
+
+        blueb.clicked[bool].connect(self.setColor)
+
+        self.square = QFrame(self)
+        self.square.setGeometry(150, 20, 100, 100)
+        self.square.setStyleSheet("QWidget { background-color: %s }" %
+                                  self.col.name())
+
+        self.setGeometry(300, 300, 280, 170)
+        self.setWindowTitle('Toggle button')
+        self.show()
+
+    def setColor(self, pressed):
+
+        source = self.sender()
+
+        if pressed:
+            val = 255
+        else:
+            val = 0
+
+        if source.text() == "Red":
+            self.col.setRed(val)
+        elif source.text() == "Green":
+            self.col.setGreen(val)
+        else:
+            self.col.setBlue(val)
+
+        self.square.setStyleSheet("QFrame { background-color: %s }" %
+                                  self.col.name())
+
+#测试20：滑块条
+class Example20(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        sld = QSlider(Qt.Horizontal, self)
+        sld.setFocusPolicy(Qt.NoFocus)
+        sld.setGeometry(30, 40, 100, 30)
+        sld.valueChanged[int].connect(self.changeValue)
+
+        self.label = QLabel(self)
+        self.label.setPixmap(QPixmap('1.jpg'))
+        # self.label.setGeometry(160, 40, 80, 30)
+        self.label.move(140,40)
+
+        self.setGeometry(300, 300, 280, 170)
+        self.setWindowTitle('QSlider')
+        self.show()
+
+    def changeValue(self, value):
+
+        if value == 0:
+            self.label.setPixmap(QPixmap('1.jpg'))
+        elif value > 0 and value <= 30:
+            self.label.setPixmap(QPixmap('2.jpg'))
+        elif value > 30 and value < 80:
+            self.label.setPixmap(QPixmap('3.jpg'))
+        else:
+            self.label.setPixmap(QPixmap('4.jpg'))
+        self.label.adjustSize()
+
+#测试21：进度条
+class Example21(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        self.pbar = QProgressBar(self)
+        self.pbar.setGeometry(30, 40, 200, 25)
+
+        self.btn = QPushButton('Start', self)
+        self.btn.move(40, 80)
+        self.btn.clicked.connect(self.doAction)
+
+        self.timer = QBasicTimer()
+        self.step = 0
+
+        self.setGeometry(300, 300, 280, 170)
+        self.setWindowTitle('QProgressBar')
+        self.show()
+
+    def timerEvent(self, e):
+
+        if self.step >= 100:
+            self.timer.stop()
+            self.btn.setText('Finished')
+            self.step=0
+            self.pbar.setValue(self.step)
+            return
+
+        self.step = self.step + 1
+        self.pbar.setValue(self.step)
+
+    def doAction(self):
+
+        if self.timer.isActive():
+            self.timer.stop()
+            self.btn.setText('Start')
+        else:
+            self.timer.start(100, self)
+            self.btn.setText('Stop')
+
+#测试22：日历组件
+class Example22(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        cal = QCalendarWidget(self)
+        cal.setGridVisible(True)
+        cal.move(20, 20)
+        cal.clicked[QDate].connect(self.showDate)
+
+        self.lbl = QLabel(self)
+        date = cal.selectedDate()
+        self.lbl.setText(date.toString())
+        self.lbl.move(130, 260)
+
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle('Calendar')
+        self.show()
+
+    def showDate(self, date):
+        self.lbl.setText(date.toString())
+
+#测试23：像素图
+class Example23(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        hbox = QHBoxLayout(self)
+        pixmap = QPixmap("2.jpg")
+
+        lbl = QLabel(self)
+        lbl.setPixmap(pixmap)
+
+        hbox.addWidget(lbl)
+        self.setLayout(hbox)
+
+        self.move(300, 200)
+        self.setWindowTitle('Red Rock')
+        self.show()
+
+#测试24：单行文本编辑框
+class Example24(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.lbl = QLabel(self)
+        qle = QLineEdit(self)
+
+        qle.move(60, 100)
+        self.lbl.move(60, 40)
+
+        qle.textChanged[str].connect(self.onChanged)
+
+        self.setGeometry(300, 300, 280, 170)
+        self.setWindowTitle('QLineEdit')
+        self.show()
+
+    def onChanged(self, text):
+        self.lbl.setText(text)
+        self.lbl.adjustSize()
+
+#测试25：分割框
+class Example25(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        hbox = QHBoxLayout(self)
+
+        topleft = QFrame(self)
+        topleft.setFrameShape(QFrame.StyledPanel)
+
+        topright = QFrame(self)
+        topright.setFrameShape(QFrame.StyledPanel)
+
+        bottom = QFrame(self)
+        bottom.setFrameShape(QFrame.StyledPanel)
+
+        splitter1 = QSplitter(Qt.Horizontal)
+        splitter1.addWidget(topleft)
+        splitter1.addWidget(topright)
+
+        splitter2 = QSplitter(Qt.Vertical)
+        splitter2.addWidget(splitter1)
+        splitter2.addWidget(bottom)
+
+        hbox.addWidget(splitter2)
+        self.setLayout(hbox)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('QSplitter')
+        self.show()
+
+#测试26：下拉列表框
+class Example26(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.lbl = QLabel("Ubuntu", self)
+
+        combo = QComboBox(self)
+        combo.addItem("Ubuntu")
+        combo.addItem("Mandriva")
+        combo.addItem("Fedora")
+        combo.addItem("Arch")
+        combo.addItem("Gentoo")
+
+        combo.move(50, 50)
+        self.lbl.move(50, 150)
+
+        combo.activated[str].connect(self.onActivated)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('QComboBox')
+        self.show()
+
+    def onActivated(self, text):
+        self.lbl.setText(text)
+        self.lbl.adjustSize()
 
 if __name__=='__main__':
-    test4()
+    app = QApplication(sys.argv)
+    ex = Example26()
+    sys.exit(app.exec_())
+    # sin = SinClass()
